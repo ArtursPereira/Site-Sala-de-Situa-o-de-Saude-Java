5b84e7e1f72a30c16de6597e80460e9860305654
Artur <artur.metallica3000@gmail.com>
2026-09-15 17:13:31 -0300

    sei la

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
         return configuration.getAuthenticationManager();
     }
 
+
     @Bean
     public SecurityFilterChain securityFilterChain(
             HttpSecurity http) throws Exception {
